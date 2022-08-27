8669efa75a821c74677a4a851bd0112c14a76496
Matt Croken <29314631+mcroken@users.noreply.github.com>
2022-08-27 01:43:38 -0400

    snpEff call

--- makefile/pathpop.mk
+++ makefile/pathpop.mk
@@ -3,24 +3,34 @@ GNOMAD_VCF_URL := https://storage.googleapis.com/gcp-public-data--gnomad/release
 GNOMAD_VCF_URL ?= https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/vcf/exomes/gnomad.exomes.r2.1.1.sites.vcf.bgz
 GNOMAD_VCF ?= $(notdir $(GNOMAD_VCF_URL))
 
+# snpEff annotated VCF
+SNPEFF_VCF ?= $(subst .vcf,snpeff.vcf,$(GNOMAD_VCF))
+
 # Genome Database used by snpEff. Must match genome of GNOMAD_VCF_URL
-GENOME_SNPEFF ?= GRCh37.75
+GENOME_SNPEFF ?= GRCh37.p13
 
 INPUT_DIR ?= input
+SNPEFF_DIR ?= snpEff
 
-$(INPUT_DIR) :
+$(INPUT_DIR) $(SNPEFF_DIR) :
 	mkdir $@
 
 $(INPUT_DIR)/$(GNOMAD_VCF) : | $(INPUT_DIR)
 	wget --output-document=$@ "$(GNOMAD_VCF_URL)" 
 
-$(INPUT_DIR)/$(GNOMAD_VCF).tbi : $(INPUT_DIR)/$(GNOMAD_VCF)
-	bcftools index --tbi $<
+%.tbi : %
+	tabix $<
 
-all : $(INPUT_DIR)/$(GNOMAD_VCF) $(INPUT_DIR)/$(GNOMAD_VCF).tbi
+$(SNPEFF_DIR)/$(SNPEFF_VCF) : $(INPUT_DIR)/$(GNOMAD_VCF) $(INPUT_DIR)/$(GNOMAD_VCF).tbi | $(SNPEFF_DIR)
+	snpEff eff -csvStats $(dir $@)summary.csv $(GENOME_SNPEFF) $< | bgzip > $@
 
-clean :
+all : $(INPUT_DIR)/$(GNOMAD_VCF) $(INPUT_DIR)/$(GNOMAD_VCF).tbi $(SNPEFF_DIR)/$(SNPEFF_VCF) $(SNPEFF_DIR)/$(SNPEFF_VCF).tbi
+
+purge : clean
 	rm -r $(INPUT_DIR)/
 
+clean :
+	rm -rf $(SNPEFF_DIR)/
 
-.PHONY : all
+
+.PHONY : all clean purge
